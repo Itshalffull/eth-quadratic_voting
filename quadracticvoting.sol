24548ef947c61545c1cb6ff8711e5c22c3efafe1
unknown <kates@KATES-T430.corp.surveymonkey.com>
2015-11-04 19:04:37 -0800

    Scaffolding

---
+++ quadracticvoting.sol
@@ -0,0 +1,16 @@
+//Quadractic voting
+contract Quadractic{
+	struct votingRules {
+		bool ethCurrency;
+		uint voteCost;
+	}
+	struct proposal {
+		bytes32 name;
+		uint voteCount;
+		uint startDate;
+		unit endDate;
+	}
+
+	function voteBuyer{
+	}
+}

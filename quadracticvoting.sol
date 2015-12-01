abbb845a6cbca904b70d2624a9dd91573caeacdf
Matt <Matt@selfmaderenegade.net>
2015-12-01 11:27:26 -0800

    Begin port to solidity

--- quadracticvoting.sol
+++ quadracticvoting.sol
@@ -1,18 +1,36 @@
-//Quadractic voting
-contract Quadractic{
-	struct votingRules {
-		bool ethCurrency;
-		uint voteCost;
-		
-	}
-	struct proposal {
-		bytes32 name;
-		uint voteCount;
-		uint startDate;
-		unit endDate;
+//Quadractic
+contract QuadracticVoting{
+	function QuadraticVoting(){
 	}
 
+//Define the basic data Structures
+	struct Voter{
+		uint public voteCount;
+		bool public yesOrNo;
+	}
+	struct Member{
+		uint[] permisions;
+	}
+	struct Proposal {
+		bytes32 public name;
+		bytes32 public description;
+		address public contract;
+		uint public yesVotes;
+		uint public noVotes;
+		uint public money;
+		uint public startDate;
+		uint public endDate;
+		mapping [address[] -> Voter] public voters;
+	}
+	struct Organization{
+		bytes32 public name;
+		uint public voteCost;
+		mapping (address[] => Member) public members;
+	}
+
+//Register an organization, and give the registering account admin access.
+	function registerOrganization{
+	}
 	function voteBuyer{
-		if votingRules.ethCurrency
 	}
 }
